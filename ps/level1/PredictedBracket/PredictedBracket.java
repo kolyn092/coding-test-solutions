00a606ec88c6e1b2cc94c4a7bf73d7be7c2f1655
kolyn092 <kolyn092@gmail.com>
2026-03-26 15:43:12 +0900

    [PGS] 예상 대진표 - ? / Level 2

---
+++ ps/level1/PredictedBracket/PredictedBracket.java
@@ -0,0 +1,37 @@
+package level1.PredictedBracket;
+
+class PredictedBracket
+{
+    public int solution(int n, int a, int b)
+    {
+        int round = 1;
+        for (int i = 0; i < n; i++) {
+            if (a % 2 == 0) {
+                if (a - 1 == b) {
+                    return round;
+                } else {
+                    a = Math.max(a, a-1)/2;
+                    if (b % 2 == 0) {
+                        b = Math.max(b, b-1)/2;
+                    } else {
+                        b = Math.max(b, b+1)/2;
+                    }
+                    round++;
+                }
+            } else {
+                if (a + 1 == b) {
+                    return round;
+                } else {
+                    a = Math.max(a, a+1)/2;
+                    if (b % 2 == 0) {
+                        b = Math.max(b, b-1)/2;
+                    } else {
+                        b = Math.max(b, b+1)/2;
+                    }
+                    round++;
+                }
+            }
+        }
+        return round;
+    }
+}
