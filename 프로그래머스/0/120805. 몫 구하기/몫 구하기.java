3fff8d29c5d678fd4782af9e54bbf1b087882f0b
kolyn092 <61215782+kolyn092@users.noreply.github.com>
2026-04-03 09:40:46 +0900

    [level 0] Title: 몫 구하기, Time: 0.03 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        try {
+            answer = num1 / num2;
+        } catch (ArithmeticException e) {
+            System.out.println("0으로 나눌 수 없습니다");
+        }
+        return answer;
+    }
+}
