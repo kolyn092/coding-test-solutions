d8ac775b3e3b1f7810d38b8bf1d9b285b7b5f061
kolyn092 <kolyn092@gmail.com>
2026-04-03 09:29:12 +0900

    [PGS] 몫 구하기 / Level 1

---
+++ ps/FindQuotient/FindQuotient.java
@@ -0,0 +1,18 @@
+package FindQuotient;
+
+public class FindQuotient {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        try {
+            answer = num1 / num2;
+        } catch (ArithmeticException e) {
+            System.out.println("0으로 나눌 수 없습니다");
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        FindQuotient findQuotient = new FindQuotient();
+        System.out.println(findQuotient.solution(1,0));
+    }
+}
