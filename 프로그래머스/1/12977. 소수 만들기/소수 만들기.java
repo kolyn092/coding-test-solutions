ab7c1c2ee80b32637dde1c3cdd3b45a6ce9070e7
kolyn092 <61215782+kolyn092@users.noreply.github.com>
2026-05-20 09:44:53 +0900

    [level 1] Title: 소수 만들기, Time: 1.37 ms, Memory: 62.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int solution(int[] nums) {
+        int count = 0;
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    
+                    int sum = nums[i] + nums[j] + nums[k];
+                    
+                    // 소수 판별
+                    if (isPrime(sum)) {
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    private boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
